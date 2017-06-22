89a535b488a6df6e42def447d7e0dbc13fc76ea1
Pavani Malli <pavanim@fb.com>
2017-06-22 14:14:24 -0700

    added in pull to refresh feature:milestone 2

--- PhotosViewController.swift
+++ PhotosViewController.swift
@@ -20,45 +20,28 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        photoTable.delegate = self // what's this for and why didn't we use it in flix?
+        //Implement pull to refresh
+        let refreshControl = UIRefreshControl()
+        
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: .valueChanged)
+        
+        photoTable.insertSubview(refreshControl, at: 0)
+    
+        photoTable.delegate = self
         photoTable.dataSource = self
         
-        print("start call to api")
-        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
-        print(url)
-        
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        print(session)
-        
-        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
-        print("configuration??")
-        
-        let task = session.dataTask(with: url) { (data,response, error) in
-            if let error = error {
-                print (error.localizedDescription)
-            } else if let data = data,
-                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                print(dataDictionary)
-                
-                let responseDictionary = dataDictionary["response"] as! [String:Any]
-                
-                self.posts = responseDictionary["posts"] as! [[String: Any]]
-                print(self.posts)
-        
-                self.photoTable.reloadData()
-                
-            }
-        
-        }
-        task.resume()
-        // Do any additional setup after loading the view.
+        fetchPosts()
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //Functions for initializing table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
     }
@@ -86,6 +69,9 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    
+    
+    //Passes in photo data to detail view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //Initialize destination view controller from segue and cast
         let photoDetailViewController = segue.destination as! PhotoDetailsViewController
@@ -96,4 +82,46 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         //Get the image of the current cell and assign to detail VC property
         photoDetailViewController.image = cell.imageDisplay.image
     }
+    
+    
+    //Pull to refresh
+    func refreshControlAction(_ refreshControl: UIRefreshControl){
+        fetchPosts()
+        
+        self.photoTable.reloadData()
+            
+        refreshControl.endRefreshing()
+    }
+    
+    //Fetches posts - network task
+    func fetchPosts() {
+        
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        print(session)
+        
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        print("configuration??")
+        
+        let task = session.dataTask(with: url) { (data,response, error) in
+            if let error = error {
+                print (error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                let responseDictionary = dataDictionary["response"] as! [String:Any]
+                
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                print(self.posts)
+                
+                self.photoTable.reloadData()
+                
+            }
+        }
+        
+        task.resume()
+    }
+    
 }
